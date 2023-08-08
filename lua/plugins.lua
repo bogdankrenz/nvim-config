c653bf7369d1d406ffc60582caf84603a3ffa14d
Bogdan Krenz <bogdan.krenz@wirbauen.digital>
2023-08-08 16:46:47 +0200

    init

---
+++ lua/plugins.lua
@@ -0,0 +1,39 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print("Packer is not installed")
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'kyazdani42/nvim-web-devicons' -- File Icons
+  use 'hoob3rt/lualine.nvim'         -- Statusline
+  use 'nvimdev/lspsaga.nvim'         -- LSP UIs
+  use 'neovim/nvim-lspconfig'        -- LSP
+  use 'onsails/lspkind-nvim'         -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer'           -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp'         -- nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp'             -- Completion
+  use 'L3MON4D3/LuaSnip'             -- Snippets
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject diagnostics, code actions, and more via Lua
+  use 'MunifTanjim/prettier.nvim'       -- Prettier plugin for Neovim's built-in LSP client
+
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+
+  use 'akinsho/nvim-bufferline.lua'
+  use 'norcalli/nvim-colorizer.lua'
+end)
