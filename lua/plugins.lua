8970f00d0dc1d58a4cf44d6d94821d8a21c67982
Bogdan Krenz <bogdan.krenz@wirbauen.digital>
2023-08-11 10:46:21 +0200

    fixed typos, added mason

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,6 +26,8 @@ packer.startup(function(use)
   }
   use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject diagnostics, code actions, and more via Lua
   use 'MunifTanjim/prettier.nvim'       -- Prettier plugin for Neovim's built-in LSP client
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
 
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
